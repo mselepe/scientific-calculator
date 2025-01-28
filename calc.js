9f7d1b22a536f5ebc2c5845e3113ea4ba54fbda3
Maselo Selepe <selepemaselo@gmail.com>
2025-01-28 12:11:45 +0200

    edited the buttons on html

--- calc.js
+++ calc.js
@@ -1,3 +1,49 @@
-function getInput() {
-    const 
+// function getInput(value) {
+//     const displayScreen = document.getElementById("input-getValue");
+//     displayScreen.value += value;
+// }
+
+// function () {}
+
+// function convertInput (equation) {
+//     var equationList = equation.split();
+
+//     for (var i=0; i<equation.length; i++) {
+//         var currentChar = equation.charAt(i);
+//         var previousChar = equation.charAt(i-1);
+
+//         if (currentChar =="(") {
+//             if (previousChar.match(/| /)) {
+                
+//             }
+//         }
+//     }
+// }
+
+// function solve(equationArray) {}
+// solve();
+
+var equation;
+
+document.getElementById("equalSign").onclick = function () {
+    equation = document.getElementById("equation").value;
+
+    var equationArray = equation.split(/| /);
+
+
+    for (var i=0; i<equation.length; i++) {
+        var currentChar = equationList.charAt(i);
+        var previousChar = equationList.charAt(i-1);
+        
+        if (!isNaN(currentChar)) { //convert digits to number
+            currentChar = Number(currentChar);
+        }
+    }
+    
+    console.log(equationList);
+    // if (currentChar == "(") {
+        
+    // }
+    
+
 }
