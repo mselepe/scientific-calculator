805e24f613e8177f8fa0be6d32d1c54a81de636f
Maselo Selepe <selepemaselo@gmail.com>
2025-01-31 16:27:27 +0200

    trying to add functionality to the buttons

--- calc.js
+++ calc.js
@@ -16,26 +16,44 @@ function convertInput (equation) {
 
         for (var i=0; i<valuesInEquation.length; i++) {
             var currentChar = valuesInEquation[i];
+            var sqrChar = String.fromCharCode("&sup");
             
-            if (!isNaN(currentChar) || currentChar==="e" || currentChar==="EE" || currentChar==="in" || currentChar.startsWith("log") || currentChar.startsWith("sin") || currentChar.startsWith("cos") || currentChar.startsWith("tan") || currentChar.startsWith("m") || currentChar.startsWith("π")) { //convert digits to number
-                if (!isNaN(currentChar)){ 
-                    convertedValue.push(Number(currentChar)); 
-                } else { 
-                    switch(currentChar) {
-                        case "π":
-                            convertedValue.push(Math.PI);
-                            break;
-                        case y:
-                            convertedValue.push("e");
-                            break;
-                        default:
-                          // code block
-                      }
+            if (currentChar[1] === "2"|| currentChar.endsWith("&sup3;")) {
+                convertedValue.push(currentChar)
+            }
+            
+            // if (!isNaN(currentChar) || currentChar==="e" || currentChar.startsWith("EE") || currentChar==="in" || currentChar.startsWith("log") || currentChar.startsWith("sin") || currentChar.startsWith("cos") || currentChar.startsWith("tan") || currentChar.startsWith("m") || currentChar.startsWith("π")) { //convert digits to number
+            //     if (!isNaN(currentChar)){ 
+            //         convertedValue.push(Number(currentChar)); 
+            //         console.log(typeof currentChar);
+            //     } else { 
+            //         switch(currentChar) {
+            //             case "π":
+            //                 convertedValue.push(Math.PI);
+            //                 break;
+            //             case "e":
+            //                 convertedValue.push(Math.E);
+            //                 break;
+            //             case "EE":
+            //                 // convertedValue.push(Math.pow(10));
+            //                 break;
+            //             case "sin":
+            //                 convertedValue.push(Math.E);
+            //                 break;
+            //             case "cos":
+            //                 convertedValue.push(Math.E);
+            //                 break;
+            //             case "tan":
+            //                 convertedValue.push(Math.E);
+            //                 break;
 
-                    convertedValue.push(currentChar); 
-                }
-                console.log(typeof currentChar);
-            } 
+            //             default:
+            //               // code block
+            //           }
+            //           convertedValue.push(currentChar); 
+
+            //     }
+            // } 
         }    
 
         var symbolsInEquation
@@ -43,6 +61,54 @@ function convertInput (equation) {
     } else {
         return "";
     }
+}
+
+
+function memoryWork(equationArray) {
+    for (var i=0; i<equationArray.length; i++) {
+        var currentChar = equationArray[i];
+
+        switch(currentChar) {
+            case "π":
+                // convertedValue.push(Math.PI);
+                currentChar = Math.PI;
+                break;
+            case "e":
+                // convertedValue.push(Math.E);
+                currentChar = Math.E;
+                break;
+            case currentChar.startsWith("EE"):
+                // convertedValue.push(Math.pow(10));
+                var power = currentChar.slice(2);
+                currentChar = Math.pow(10, power);
+                equationArray.splice(power, 1);
+                break;
+            case "sinh":
+                var param = currentChar.slice(2);
+                currentChar = Math.pow(10, param);
+                equationArray.splice(param, 1);
+
+                break;
+            case "cosh":
+                var param = currentChar.slice(2);
+                currentChar = Math.pow(10, param);
+                equationArray.splice(param, 1);
+
+                break;
+            case "tanh":
+                var param = currentChar.slice(2);
+                currentChar = Math.pow(10, param);
+                equationArray.splice(param, 1);
+                break;
+
+            default:
+                // code block
+            }
+
+        convertedValue.push(currentChar); 
+    }
+    console.log(typeof currentChar);
+    
 }
 
 // function solve(equationArray) {}
@@ -72,6 +138,11 @@ function displayAnswer(answer) {
     document.getElementById("equation").value = answer;
 }
 
+function displayValue(value) {
+    var display = document.getElementById("equation");
+    display.value += value; 
+}
+
 document.getElementById("allClear").onclick = function () {
     document.getElementById("equation").value = "";
     
@@ -81,7 +152,3 @@ document.getElementById("allClear").onclick = function () {
 //     document.getElementById("equation").value = "mc";
     
 // }
-function displayValue (value) {
-    var display = document.getElementById("equation");
-    display.value += value; 
-}
