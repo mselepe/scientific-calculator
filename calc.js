dd658fc52aa98375d268052911e8355c690566bd
Maselo Selepe <selepemaselo@gmail.com>
2025-01-31 14:11:22 +0200

    Added functionality of the buttons

--- calc.js
+++ calc.js
@@ -5,45 +5,83 @@
 
 // function () {}
 
-// function convertInput (equation) {
-//     var equationList = equation.split();
+function convertInput (equation) {
+    var convertedEq = new Array();
+    
+    // Convert the numbers
+    if (equation.length > 0) {
+        var valuesInEquation = equation.split(/[-+/x]/);
+        var convertedValue = new Array();
+        var symbols = new Array();
 
-//     for (var i=0; i<equation.length; i++) {
-//         var currentChar = equation.charAt(i);
-//         var previousChar = equation.charAt(i-1);
+        for (var i=0; i<valuesInEquation.length; i++) {
+            var currentChar = valuesInEquation[i];
+            
+            if (!isNaN(currentChar) || currentChar==="e" || currentChar==="EE" || currentChar==="in" || currentChar.startsWith("log") || currentChar.startsWith("sin") || currentChar.startsWith("cos") || currentChar.startsWith("tan") || currentChar.startsWith("m") || currentChar.startsWith("π")) { //convert digits to number
+                if (!isNaN(currentChar)){ 
+                    convertedValue.push(Number(currentChar)); 
+                } else { 
+                    switch(currentChar) {
+                        case "π":
+                            convertedValue.push(Math.PI);
+                            break;
+                        case y:
+                            convertedValue.push("e");
+                            break;
+                        default:
+                          // code block
+                      }
 
-//         if (currentChar =="(") {
-//             if (previousChar.match(/| /)) {
-                
-//             }
-//         }
-//     }
-// }
+                    convertedValue.push(currentChar); 
+                }
+                console.log(typeof currentChar);
+            } 
+        }    
+
+        var symbolsInEquation
+        return convertedValue;
+    } else {
+        return "";
+    }
+}
 
 // function solve(equationArray) {}
 // solve();
 
+
+
 var equation;
 
 document.getElementById("equalSign").onclick = function () {
+    // var box = document.getElementById("equation");
     equation = document.getElementById("equation").value;
+    
+    var convertedEquation = convertInput(equation);
+    // for (var i=0; i<convertedEquation.length; i++) {
+    //     var t = convertedEquation[i];
+    //     if (!isNaN(t)) {
 
-    var equationArray = equation.split(/| /);
-
-
-    // for (var i=0; i<equation.length; i++) {
-    //     var currentChar = equationArray.charAt(i);
-    //     var previousChar = equationArray.charAt(i-1);
-        
-    //     if (!isNaN(currentChar)) { //convert digits to number
-    //         currentChar = Number(currentChar);
     //     }
     // }
     
-    console.log(typeof equationArray);
-    // if (currentChar == "(") {
-        
-    // }
-    
+    displayAnswer(convertedEquation);
+    console.log(convertedEquation);
+}
 
+function displayAnswer(answer) {
+    document.getElementById("equation").value = answer;
+}
+
+document.getElementById("allClear").onclick = function () {
+    document.getElementById("equation").value = "";
+    
+}
+
+// document.getElementById("mc").onclick = function () {
+//     document.getElementById("equation").value = "mc";
+    
+// }
+function displayValue (value) {
+    var display = document.getElementById("equation");
+    display.value += value; 
 }
